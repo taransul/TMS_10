b110b7158ea369e792174f9df4d5ef6a5fb5c1a8
taransul <taransul@yandex.by>
2021-09-25 15:09:59 +0300

    (Done 14) added in class Delivery (val deliver: (Order)->Unit)

--- app/src/main/java/com/example/tms_10/Delivery.kt
+++ app/src/main/java/com/example/tms_10/Delivery.kt
@@ -1,8 +1,18 @@
 package com.example.tms_10
 
 // DONE 13
-sealed class Delivery {
-    data class BikeDelivery(val bike: String) : Delivery()
-    data class CarDelivery(val car: String) : Delivery()
-    data class PostDelivery(val post: String) : Delivery()
+sealed class Delivery(
+    val deliver: (Order) -> Unit // DONE 14
+) {
+    data class BikeDelivery(val bike: String) : Delivery({
+        println("Bike delivery of order ${it.id} using bike $bike")  // DONE 14
+    })
+
+    data class CarDelivery(val car: String) : Delivery({
+        println("$car delivered your order ${it.id}")  // DONE 14
+    })
+
+    data class PostDelivery(val post: String) : Delivery({
+        println("$post had lost you order ${it.id} and stolen your ${it.items}")  // DONE 14
+    })
 }
