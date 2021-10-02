1a97cd8655abd4e1db527826eb8afa94939d83c6
taransul <taransul@yandex.by>
2021-10-02 22:06:26 +0300

    DONE 1

---
+++ app/src/main/java/com/example/tms_10/homework_18/Discipline.kt
@@ -0,0 +1,4 @@
+package com.example.tms_10.homework_18
+
+class Discipline(val title: String, val id: Int) {
+}
---
+++ app/src/main/java/com/example/tms_10/homework_18/Student.kt
@@ -0,0 +1,4 @@
+package com.example.tms_10.homework_18
+
+class Student(val name: String, val id: Int) {
+}
